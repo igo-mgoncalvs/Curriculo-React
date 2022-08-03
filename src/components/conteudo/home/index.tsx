1694125da20066bb8d2d33ce6e5cef70eba8c983
unknown <adminpoli@POLI.local>
2022-08-03 10:58:29 -0300

    Terminei o home o próximo é o componente de contatos

--- src/components/conteudo/home/index.tsx
+++ src/components/conteudo/home/index.tsx
@@ -1,9 +1,11 @@
 import styles from './home.module.scss'
 
 export default function Home () {
+    const imgURL = 'https://github.com/igo-mgoncalvs/Curriculo-React/blob/master/src/assets/home/IMG-20210718-WA0007.png?raw=true'
+    
     return (
         <div className={styles.home}>
-            teste
+            <img src={imgURL} alt="Minha foto" />
         </div>
     )
 }
